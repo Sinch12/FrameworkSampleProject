bf7f23ec87146349ba262db6b79a59142a33c6cc
Sinchana K S <Sinchana K S@LAPTOP-8SEKENFG>
2024-12-20 14:48:49 +0530

    modified

--- SeleniumPractice/src/test/java/pack2/OrgTest.java
+++ SeleniumPractice/src/test/java/pack2/OrgTest.java
@@ -6,6 +6,14 @@ public class OrgTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("execute createOrgTest");
+		String url = System.getProperty("URL");
+		String browser = System.getProperty("BROWSER");
+		String username = System.getProperty("USERNAME");
+		String password = System.getProperty("PASSWORD");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
 	}
 	@Test
 	public void modifyOrgTest() {
